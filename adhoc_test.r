183f37145b22669ff9dd15ee703ee556f06b17c8
tpob <tpob@tpob.xyz>
2021-12-08 23:46:24 +0800

    remove old code

--- adhoc_test.r
+++ adhoc_test.r
@@ -1,8 +1,7 @@
 devtools::load_all()
 t1<-read_computed_matrix("data/cgi_sorted_mainchr_kate_chip.mtx.gz")
-t1$sample_matrix[1]
 t1$metadata
-t1$calculate_mean_coverage()
+t1$calculate_mean_coverage_each_sample()
 t1$sample_mean_coverage
 t1$calculate_total_coverage_each_region()
 t1$region_total_coverage
